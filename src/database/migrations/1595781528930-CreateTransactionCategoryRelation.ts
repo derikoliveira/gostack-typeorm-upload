1db0a02a65b89a919af662cebc745f5a98181984
Derik Oliveira <derikoliveira03@gmail.com>
2020-07-26 16:20:20 -0300

    Criando migrations, alterando model transaction e configuração eslint

---
+++ src/database/migrations/1595781528930-CreateTransactionCategoryRelation.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateTransactionCategoryRelation1595781528930
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'transactions_category',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'transactions_category');
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
